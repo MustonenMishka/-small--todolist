f8ead3232e58bafdb9c6b860cfea572b8e0b3198
Mikhail <mustonen94m@gmail.com>
2020-09-06 10:26:30 +0300

    fix problems with heroku hosting 2

--- app.js
+++ app.js
@@ -125,11 +125,4 @@ app.get("/about", function (req, res) {
     res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
-
-app.listen(port, function () {
-    console.log("Server started");
-});
+app.listen(process.env.PORT || 5000);
